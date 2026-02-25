c9ad7e6b5b5deb3bd6eec8c7fdbab39e9df43572
djangogirls <you@example.com>
2026-02-25 22:11:26 +0600

    create the hospitals Single page

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('doctor/', views.doctor_single, name='doctor_single'),
+    path('hospital/', views.hospital_single, name='hospital_single'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'myapp/index.html')
+
+def doctor_single(request):
+    return render(request, 'myapp/doctors_single.html')
+
+def hospital_single(request):
+    return render(request, 'myapp/hospital_single.html')
